6c694954c27a69e9f01b186a1a0e5eb7457b65bf
Alexey-Pavlov <pavlov.aleksey.m@gmail.com>
2023-11-21 23:00:55 +0100

    Add chessboard with clickable squares

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import TheChessboard from '@/components/TheChessboard.vue'
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <!--    <div class="wrapper">-->
+    <!--      <HelloWorld msg="You did it!" />-->
+    <!--    </div>-->
   </header>
 
   <main>
-    <TheWelcome />
+    <!--    <TheWelcome />-->
+    <TheChessboard />
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+
+main {
+  width: 100%;
+  height: 100%;
 }
 
 .logo {
---
+++ src/components/TheChessboard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// const lastClickedSquare = ref(null)
+
+function handleClick(square: number) {
+  // lastClickedSquare.value = square
+  console.log(square)
+  // Additional logic for highlighting or other functionalities
+}
+
+function getSquareColor(index: number) {
+  const x = (index - 1) % 8
+  const y = Math.floor((index - 1) / 8)
+  return (x + y) % 2 === 0 ? 'white-square' : 'black-square'
+}
+</script>
+
+<template>
+  <div class="chessboard">
+    <div
+      v-for="index in 64"
+      :key="index"
+      class="chess-square"
+      :class="getSquareColor(index)"
+      @click="handleClick(index)"
+    ></div>
+  </div>
+</template>
+
+<style scoped>
+.chessboard {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  max-width: 100%;
+  aspect-ratio: 1;
+  max-height: 90vh;
+}
+
+.chess-square {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #333;
+}
+
+.white-square {
+  background-color: #fff;
+}
+
+.black-square {
+  background-color: #000;
+}
+</style>
